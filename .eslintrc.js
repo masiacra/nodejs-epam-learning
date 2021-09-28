a86881593572d9c3a713971a66aa4c33add143c2
igor_savelev1@epam.com <igor_savelev1@epam.com>
2021-09-28 16:20:38 +0300

    Setup eslint
      * install eslint;
      * add .eslintrs.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        browser: false,
+        node: true,
+        es2021: true,
+    },
+    extends: ['node'],
+    parserOptions: {
+        ecmaFeatures: {},
+        ecmaVersion: 12,
+        sourceType: 'commonjs',
+    },
+    plugins: [],
+    rules: {},
+};
