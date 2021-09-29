75d75b9c0d1e4d9c068cacb10783646b3ec0f874
igor_savelev1@epam.com <igor_savelev1@epam.com>
2021-09-29 09:29:04 +0300

    Add reverseString task
      * create new folder;
      * add new helper reverseString;
      * add main functionality;
      * add new script to package.json

---
+++ src/reverse/index.js
@@ -0,0 +1,15 @@
+const { createInterface } = require('readline');
+const { reverseString } = require('./helpers');
+
+const main = () => {
+    const readline = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    readline.on('line', (chunk) => {
+        console.log(reverseString(chunk));
+    });
+};
+
+main();
